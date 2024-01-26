b13aefc64ca651b73ade8a73641083e49c1015e7
Kervens Gertilus <kervensgertilus@kervenss-air.home>
2024-01-25 22:12:29 -0500

    added a new file

---
+++ index.js
@@ -0,0 +1,15 @@
+alert ( "Please enter the first name you would like to be compared. ")
+ var nameOne;
+alert ("Please enter the second name you would like to be compared. ")
+var nameTwo;
+
+function names(nameOne, nameTwo){
+
+    var a = Math.random(a);
+        a = a * 99;
+        a = Math.floor(a) +1;
+
+    console.log("The percentage of of your love score is:" + a + "%") ;
+}
+
+
