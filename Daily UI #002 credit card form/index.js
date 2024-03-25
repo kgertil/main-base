022fcf57947379709a7ad6399b90993046dc8a01
kgertil <84053359+kgertil@users.noreply.github.com>
2024-03-24 20:32:18 -0400

    Add files via upload

---
+++ Daily UI #002 credit card form/index.js
@@ -0,0 +1,16 @@
+var bellBoy ( name, age, PermissionStatus, speaks, experience, cleaning ){
+this.name = name;
+this.age = age;
+this.PermissionStatus = PermissionStatus; 
+this.speaks = speaks;
+this.cleaning = cleaning;
+this.experience= experience;
+ 
+ };
+
+
+ // constructor function
+
+function bellBoyalt= new BellBoy1 ( "Jim", 26, True, ["French", "English", "Spanisg"], "2 years", ["Bedroom", "Bathroom", "Lobby"] );
+ console.log (bellBoy.name);
+
